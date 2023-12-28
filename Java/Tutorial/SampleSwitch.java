29f079d45cfa54df7131a21900b6a91a581d015c
DESKTOP-IDTA4TV\Dinesh (RDX) <dineshrd111@gmail.com>
2023-12-28 20:35:06 +0530

    sampleswitch added

---
+++ Java/Tutorial/SampleSwitch.java
@@ -0,0 +1,16 @@
+class SampleSwitch{
+public static void main(String args[]){
+for(int i=0;i<4;i++){
+switch(i){
+case 0:
+System.out.println("i is zero.");
+break;
+case 1:
+System.out.println("i is one.");
+break;
+default:
+System.out.println("i is greater than 1.");
+}
+}
+}
+}
